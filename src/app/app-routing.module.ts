0b2dea1e883f93354942ac55c9e630924fe5187d
Anush Adhikari <aadhikari@ceiamerica.com>
2022-09-09 13:29:35 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'chatroom', component: ChatroomComponent },
+  {path: '',
+redirectTo: 'chatroom',
+pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
